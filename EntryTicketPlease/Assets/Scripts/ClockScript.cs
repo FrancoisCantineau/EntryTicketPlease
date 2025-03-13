41a330c36d28e427fd32d2bcc0ede752385e647b
cbobeda <cbobeda@gaming.tech>
2025-03-13 23:53:57 +0100

    Date struct
    implementation d'un struct pour géré la date

--- EntryTicketPlease/Assets/Scripts/ClockScript.cs
+++ EntryTicketPlease/Assets/Scripts/ClockScript.cs
@@ -7,17 +7,17 @@ using UnityEngine;
 public class ClockScript : MonoBehaviour
 {
     [SerializeField] int nbSeconds;
-    [SerializeField] int year;
-    [SerializeField] int month;
-    [SerializeField] int day;
     [SerializeField] TextMeshProUGUI affichage;
     private float ratio;
     DateTime time;
-    // Start is called before the first frame update
+
+    [SerializeField] Date date;
+
+
     void Start()
     {
         ratio = 600 / nbSeconds;
-        time = new DateTime(year, month, day, 8, 0, 0);
+        time = new DateTime(date.year, date.month, date.day, 8, 0, 0);
     }
 
     // Update is called once per frame
@@ -26,4 +26,9 @@ public class ClockScript : MonoBehaviour
         time = time.AddMinutes(ratio * Time.deltaTime);
         affichage.text = time.ToString("H:mm dd/MM/yyyy");
     }
+}
+
+public struct Date
+{
+    public int year, month, day;
 }
--- EntryTicketPlease/Assets/Scripts/WinLoseText.cs
+++ EntryTicketPlease/Assets/Scripts/WinLoseText.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinLoseText : MonoBehaviour
 {
     [SerializeField] bool isWinning;
     [SerializeField] TextMeshProUGUI winLosetext;
+    [SerializeField] RawImage star1;
+    [SerializeField] RawImage star2;
+    [SerializeField] RawImage star3;
+    [SerializeField] int nbStars;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,28 @@ public class WinLoseText : MonoBehaviour
         {
             winLosetext.text = "You lose";
         }
+        star1.enabled = false;
+        star2.enabled = false;
+        star3.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (nbStars)
+        {
+            case 1:
+                star1.enabled = true;
+                break;
+            case 2:
+                star1.enabled = true;
+                star2.enabled = true;
+                break;
+            case 3:
+                star1.enabled= true;
+                star2.enabled= true;
+                star3.enabled= true;
+                break;
+        }
     }
 }
