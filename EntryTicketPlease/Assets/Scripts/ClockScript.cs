6174e5f7ea4f243fa61595fa5b085fe6def0d074
scatanese <scatanese@GCFRPA091>
2025-03-14 18:42:50 +0100

    wip

--- EntryTicketPlease/Assets/Scripts/ClockScript.cs
+++ EntryTicketPlease/Assets/Scripts/ClockScript.cs
@@ -13,10 +13,14 @@ public class ClockScript : MonoBehaviour
     [SerializeField] int duration;
     [SerializeField] int startHour;
     [SerializeField] DateTime date;
-    [SerializeField] int timewarnning;
+    [SerializeField] int timewarnning = 2;
+    bool isTimeLow = false;
     public UnityEvent<int> AlmostFinished = new();
     public UnityEvent Finished = new();
     private float ratio;
+
+    [SerializeField] GameObject timesUpObject;
+    [SerializeField] GameObject timeLowObject;
 
 
     public enum Season
@@ -35,29 +39,61 @@ public class ClockScript : MonoBehaviour
         Dec,
     }
 
+    private void OnEnable()
+    {
+        StartCoroutine(PlayOpening());
+    }
+
 
     void Start()
     {
+        var roundData = GameManager.CurrentRoundData;
+        duration = roundData.shiftLength;
+        startHour = roundData.startingHour;
+
+        nbSeconds = (int)GameSettings.HourDurationSeconds * duration;
         ratio = (duration * 60) / nbSeconds;
         GameTime = date;
         TimeSpan ts = new TimeSpan(startHour, 0, 0);
         GameTime = GameTime.Date + ts;
         calandar.text = (Season)(GameTime.Month - 1) + "<br>" + GameTime.Day.ToString();
+        StartCoroutine(Countdown());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Countdown()
     {
-        GameTime = GameTime.AddMinutes(ratio * Time.deltaTime);
-        affichage.text = GameTime.ToString("H:mm");
-        if (GameTime.Hour == startHour + duration - timewarnning)
+        while (GameTime.Hour < startHour + duration)
         {
-            AlmostFinished.Invoke(timewarnning);
+            yield return null;
+            GameTime = GameTime.AddMinutes(ratio * Time.deltaTime);
+            affichage.text = GameTime.ToString("H:mm");
+            if (GameTime.Hour > startHour + duration - timewarnning && !isTimeLow)
+            {
+                AlmostFinished.Invoke(timewarnning);
+                isTimeLow = true;
+                timeLowObject.SetActive(true);
+            }
         }
-        if (GameTime.Hour == startHour + duration)
-        {
-            Finished.Invoke();
-        }
+        Finished.Invoke();
+        timesUpObject.SetActive(true);
     }
     public DateTime GameTime { get; private set; }
+
+
+
+
+    IEnumerator PlayOpening()
+    {
+        var TargetPosition = transform.position;
+        var EnterPosition = new Vector3(TargetPosition.x + 800, TargetPosition.y, TargetPosition.z);
+        transform.position = EnterPosition;
+        float duration = 1f;
+        float time = 0;
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(EnterPosition, TargetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
